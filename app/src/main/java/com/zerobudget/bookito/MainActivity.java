c0f992fcebc2cb48dfbdb3f8e77b26171f985b84
Diego <diegopassarella01@gmail.com>
2022-11-13 20:16:45 +0100

    implemented profile fragment

--- app/src/main/java/com/zerobudget/bookito/MainActivity.java
+++ app/src/main/java/com/zerobudget/bookito/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,25 +55,32 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.request_page_nav, R.id.to_navigation_library, R.id.navigation_search) //changed navigation_requests to request_page_nav
                 .build();
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
     @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.profile:
-                Toast.makeText(getApplicationContext(), "Premuto", Toast.LENGTH_SHORT).show();
-                return true;
+            case R.id.userProfileFragment:
+                NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+                return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
 
             case R.id.logout:
                 // TODO: Logout utente
                 // FirebaseAuth.getInstance().signOut();
-                // Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
 
             default:
                 return super.onOptionsItemSelected(item);
--- app/src/main/java/com/zerobudget/bookito/ui/profile/UserProfileFragment.java
+++ app/src/main/java/com/zerobudget/bookito/ui/profile/UserProfileFragment.java
@@ -1,8 +1,51 @@
 package com.zerobudget.bookito.ui.profile;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.zerobudget.bookito.databinding.FragmentUserProfileBinding;
 
 public class UserProfileFragment extends Fragment {
 
+    private FragmentUserProfileBinding binding;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+
+    // BottomNavigationView navBar;
+
+    public UserProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentUserProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        // navBar = getActivity().findViewById(R.id.nav_view);
+        // navBar.setVisibility(View.INVISIBLE);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // navBar.setVisibility(View.VISIBLE);
+        binding = null;
+    }
 
 }
