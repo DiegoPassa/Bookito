42d29de83db4ca68ac12e01d505350cdc2e80eed
Diego Passarella <diegopassarella01@gmail.com>
2022-11-16 12:25:15 +0100

    fixed merge errors

--- app/src/main/java/com/zerobudget/bookito/login/OTPConfirmFragment.java
+++ app/src/main/java/com/zerobudget/bookito/login/OTPConfirmFragment.java
@@ -2,15 +2,14 @@ package com.zerobudget.bookito.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,8 +23,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.zerobudget.bookito.MainActivity;
 import com.zerobudget.bookito.R;
 import com.zerobudget.bookito.databinding.FragmentOtpConfirmBinding;
-import com.zerobudget.bookito.ui.library.BookModel;
-import com.zerobudget.bookito.ui.users.UserModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
