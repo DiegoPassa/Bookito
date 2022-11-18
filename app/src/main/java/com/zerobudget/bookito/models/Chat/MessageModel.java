9e44b10b265c2e03e4b19fd60f7064a6a1813920
Chineee <886217@stud.unive.it>
2022-11-18 18:47:47 +0100

    Implemented some methods of MessageModel
    Signed-off-by: Chineee <886217@stud.unive.it>

--- app/src/main/java/com/zerobudget/bookito/models/Chat/MessageModel.java
+++ app/src/main/java/com/zerobudget/bookito/models/Chat/MessageModel.java
@@ -1,13 +1,53 @@
 package com.zerobudget.bookito.models.Chat;
 
+import java.util.ArrayList;
+
 public class MessageModel {
     private String sender;
     private String receiver;
     private String message;
+    private String messageId;
 
     public MessageModel(String sender, String receiver, String message) {
         this.sender = sender;
         this.receiver = receiver;
         this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public static ArrayList<MessageModel> getMessages(String user1, String user2) {
+        //TODO get un array of message between two users (maybe could be useful?)
+        return null;
     }
 }
