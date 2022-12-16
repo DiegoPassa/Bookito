03465f19bc747a88f4b1c32d24da0c3730457377
Giulia Cogotti <cogotti.giulia.irl@gmail.com>
2022-12-16 13:18:21 +0100

    added badge in accepted requests tab counting the total of not read messages

--- app/src/main/java/com/zerobudget/bookito/ui/inbox/RequestFragment.java
+++ app/src/main/java/com/zerobudget/bookito/ui/inbox/RequestFragment.java
@@ -1,6 +1,7 @@
 package com.zerobudget.bookito.ui.inbox;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +10,21 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.zerobudget.bookito.R;
+import com.zerobudget.bookito.utils.Utils;
 
 import java.util.Objects;
 
 public class RequestFragment extends Fragment {
 
-    ViewPager2 viewPager;
-    TabLayout tabs;
+    private DatabaseReference realTimedb;
+    private ViewPager2 viewPager;
+    private TabLayout tabs;
 
     RequestPageAdapter adapter;
 
@@ -31,6 +39,7 @@ public class RequestFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_request_page, container, false);
 
+        realTimedb = FirebaseDatabase.getInstance().getReference("/chatapp/");
 
         // Setting ViewPager for each Tabs
         viewPager = view.findViewById(R.id.viewPager);
@@ -41,22 +50,28 @@ public class RequestFragment extends Fragment {
         tabs.getTabAt(Wrapper.position).select();
         viewPager.setCurrentItem(Wrapper.position);
 
+        setUpBadging();
 
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                //il badge è rimosso se si è nel tab corrente
+                tab.removeBadge();
                 viewPager.setCurrentItem(tab.getPosition());
                 Wrapper.setPosition(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                //se non è selezionato l'ultimo tab mostra il badge
+                if (tab.getPosition() == 2)
+                    setUpBadging();
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
+                //il badge è rimosso se si riseleziona il tab
+                tab.removeBadge();
             }
         });
 
@@ -89,6 +104,41 @@ public class RequestFragment extends Fragment {
         public static void setPosition(Integer i) {
             position = i;
         }
+    }
+
+    /**
+     * in real time vede se esistono nuovi messaggi nelle chat e ne visualizza il numero nel tab realtivo
+     */
+    protected void setUpBadging() {
+
+        realTimedb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                int tot = 0;
+                //TODO dare permessi migliori su firebase
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        if (!ds.getKey().equals("user1") && !ds.getKey().equals("user2")) {
+                            //se lo status del messaggio dell'altro utente è segnato come sent,
+                            //viene contato come nuovo messaggio
+                            if (ds.hasChild("status"))
+                                if (ds.child("receiver").getValue(String.class).equals(Utils.USER_ID)
+                                        && ds.child("status").getValue(String.class).equals("sent"))
+                                    tot++;
+                        }
+                    }
+                }
+                if(tot > 0)
+                    tabs.getTabAt(2).getOrCreateBadge().setNumber(tot);
+                else
+                    tabs.getTabAt(2).removeBadge();
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+                Log.e("DB ERROR", error.getMessage());
+            }
+        });
     }
 
 
