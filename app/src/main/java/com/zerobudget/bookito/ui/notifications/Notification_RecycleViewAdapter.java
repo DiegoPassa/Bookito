f3dc3b1b76985c6b9ae4788ad1edd8e0f6439434
Diego <diegopassarella01@gmail.com>
2023-01-29 19:51:12 +0100

    fixed gravatar not showing on Notification Fragment

--- app/src/main/java/com/zerobudget/bookito/ui/notifications/Notification_RecycleViewAdapter.java
+++ app/src/main/java/com/zerobudget/bookito/ui/notifications/Notification_RecycleViewAdapter.java
@@ -9,10 +9,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.storage.FirebaseStorage;
+import com.lelloman.identicon.view.ClassicIdenticonView;
 import com.squareup.picasso.Picasso;
 import com.zerobudget.bookito.R;
 import com.zerobudget.bookito.models.notification.NotificationModel;
@@ -51,15 +51,22 @@ public class Notification_RecycleViewAdapter extends RecyclerView.Adapter<Notifi
 
             holder.date.setText(elapsedTime);
         }
+
         FirebaseStorage.getInstance().getReference()
                 .child("profile_pics/")
                 .child(notification.get(position).getActionerId())
                 .getDownloadUrl()
                 .addOnSuccessListener(uri ->
                         Picasso.get().load(uri).into(holder.actioner_image)
-                );
+                )
+                .addOnFailureListener(runnable -> {
+                    holder.actioner_gravatar.setHash(notification.get(position).getActioner().getTelephone().hashCode());
+                    holder.actioner_gravatar.setVisibility(View.VISIBLE);
+                    holder.actioner_image.setVisibility(View.GONE);
+                });
 
         Picasso.get().load(notification.get(position).getBook_thumb()).into(holder.book_victim);
+
     }
 
 
@@ -73,8 +80,8 @@ public class Notification_RecycleViewAdapter extends RecyclerView.Adapter<Notifi
         private final TextView title;
         private final TextView body;
         private final ImageView book_victim;
-        private final ConstraintLayout item_selected;
         private final ImageView actioner_image;
+        private final ClassicIdenticonView actioner_gravatar;
         private final TextView date;
 
         public ViewHolder(@NonNull View itemView) {
@@ -83,9 +90,9 @@ public class Notification_RecycleViewAdapter extends RecyclerView.Adapter<Notifi
             title = itemView.findViewById(R.id.not_title);
             body = itemView.findViewById(R.id.not_body);
             book_victim = itemView.findViewById(R.id.book_victim);
-            item_selected = itemView.findViewById(R.id.item_selected);
             actioner_image = itemView.findViewById(R.id.actioner_image);
             date = itemView.findViewById(R.id.date);
+            actioner_gravatar = itemView.findViewById(R.id.user1_gravatar);
         }
     }
 
